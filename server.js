26c97a79f7cafd47fcd1ba0b31bc08e03b4ce8b5
h53288514 <h53288514@gmail.com>
2025-08-03 18:11:29 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("tera code run hogya");
+const { execFile } = require('child_process');
+
+execFile('./popatlal.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error('Error executing script:', error);
+    return;
+  }
+  console.log('stdout:', stdout);
+  console.error('stderr:', stderr);
+});
